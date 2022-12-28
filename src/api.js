958bf221e7d83f572fdb61351d722c0e941d7a8a
SudhanshuTomar <sudhanshutomar007@gmail.com>
2022-12-28 14:11:52 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a898b61a65mshb69430fc7e96985p19fdf5jsn1efe798dd170",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8f4f22e0ab479a43df4342120db70c9a"; // enter your key from openweather API
